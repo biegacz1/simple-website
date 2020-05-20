354d2256c4f1663e535f1fe0fd50be9e1ed9135e
biegacz <majquel@gmail.com>
2020-05-20 15:44:44 +0200

    db credentials

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?
 
+const DB_HEROKU = env('DATABASE_URL');
 const DB_DSN="pgsql:host=ec2-54-247-94-127.eu-west-1.compute.amazonaws.com;dbname=de2mknpcc30d5o";
 const DB_USER="ywvxfwpsjqweip";
 const DB_PASS="9a187c0c365d8f260648f1592bebbf26ca5e153a309a2c849c3e68d18154ba74";
@@ -11,7 +12,7 @@ const OPTIONS = [
 
 function getConn() {
     try {
-        return new PDO( DB_DSN, DB_USER, DB_PASS, OPTIONS);
+        return new PDO( DB_HEROKU, DB_USER, DB_PASS, OPTIONS);
     } catch (\PDOException $e) {
         throw new \PDOException($e->getMessage(), (int)$e->getCode());
     }
