b73bb99e2197348d70d5f47af59311c72791c1bc
biegacz <majquel@gmail.com>
2020-05-20 15:53:03 +0200

    pdo conn

--- db.php
+++ db.php
@@ -1,6 +1,15 @@
 <?
+$db_url = parse_url(getenv("DATABASE_URL"));
 
-$DB_HEROKU = constant($_ENV["DATABASE_URL"]);
+$PDO = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db_url["host"],
+    $db_url["port"],
+    $db_url["user"],
+    $db_url["pass"],
+    ltrim($db_url["path"], "/")
+));
+
 const DB_DSN="pgsql:host=ec2-54-247-94-127.eu-west-1.compute.amazonaws.com;dbname=de2mknpcc30d5o";
 const DB_USER="ywvxfwpsjqweip";
 const DB_PASS="9a187c0c365d8f260648f1592bebbf26ca5e153a309a2c849c3e68d18154ba74";
@@ -12,7 +21,7 @@ const OPTIONS = [
 
 function getConn() {
     try {
-        return new PDO( $DB_HEROKU, DB_USER, DB_PASS, OPTIONS);
+        return new PDO( $PDO, DB_USER, DB_PASS, OPTIONS);
     } catch (\PDOException $e) {
         throw new \PDOException($e->getMessage(), (int)$e->getCode());
     }
