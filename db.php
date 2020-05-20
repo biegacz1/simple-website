51fb583f317003451700a8f09b3be07c48a85e5c
biegacz <majquel@gmail.com>
2020-05-20 15:59:25 +0200

    fix

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
 <?
-$db_url = parse_url(getenv("DATABASE_URL"));
-const OPTIONS = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
 function getConn() {
     try {
+        $db_url = parse_url(getenv("DATABASE_URL"));
+
+        const OPTIONS = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
         return new PDO("pgsql:" . sprintf(
             "host=%s;port=%s;user=%s;password=%s;dbname=%s",
             $db_url["host"],
