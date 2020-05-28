583de7337ffb5171e4bc387542a725ea60643e71
biegacz <majquel@gmail.com>
2020-05-28 20:17:22 +0200

    added styles

--- templates/post/index.php
+++ templates/post/index.php
@@ -6,21 +6,30 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
     <title>Posty</title>
 </head>
 <body>
 
 <div class="container">
-    <div class="mt-5">
+    <div class="mt-5 mb-2">
         <h1>Posty</h1>
+        <div>
+            <a class="list-group-item list-group-item-action" href="post/<? echo $result['id'] ?>">
+                <button type="button" class="close" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </a>
+        </div>
         <ul>
             <? foreach ($posts as $result) { ?>
                 <li><a href="post/<? echo $result['id'] ?>"><? echo $result['title'] ?></a></li>
             <? } ?>
         </ul>
     </div>
+
     <div class="row">
-        <a href="/create">Dodaj nowy</a>
+        <a class="btn btn-primary btn-lg active" role="button" aria-pressed="true" href="/create">Dodaj nowy</a>
     </div>
 </div>
 
