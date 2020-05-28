95433f4bac1d8023908e5c5b9fd1c0e98df5ba9f
biegacz <majquel@gmail.com>
2020-05-28 20:40:42 +0200

    added placehodlers

--- templates/post/edit.php
+++ templates/post/edit.php
@@ -4,23 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../style.css">
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="../../style.css">
     <title>Document</title>
 </head>
 <body>
 <div class="container">
     <div class="card mt-5">
-        <form action="/update/<? echo $post["id"] ?>" method="post">
+        <form action="/update/<? echo $post["id"] ?>"
+              method="post">
             <div class="card-header">
                 <h2>
-                    <input type="text" name="title" class="form-control" value="<? echo $post["title"] ?>">
+                    <input type="text"
+                           name="title"
+                           placeholder="Podaj tytuł posta"
+                           class="form-control"
+                           value="<? echo $post["title"] ?>">
                 </h2>
             </div>
             <div class="card-body">
-                <textarea class="form-control" name="content" id="content" cols="30" rows="10"><? echo $post["content"] ?></textarea>
+                <textarea class="form-control"
+                          placeholder="Podaj treść posta"
+                          name="content"
+                          id="content"
+                          cols="30"
+                          rows="10"><? echo $post["content"] ?></textarea>
                 <button class="btn btn-success  mt-2">Zaktualizuj</button>
             </div>
         </form>
--- templates/post/post.php
+++ templates/post/post.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../style.css">
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="../../style.css">
     <title>Document</title>
 </head>
 <body>
@@ -16,12 +20,13 @@
         <div class="card-header">
             <h2>
                 <? echo $post["title"] ?>
-                <a href="/edit/<? echo $post["id"] ?>" class="btn btn-primary float-right">Edytuj</a>
+                <a href="/edit/<? echo $post["id"] ?>"
+                   class="btn btn-primary float-right">Edytuj</a>
             </h2>
         </div>
         <div class="card-body">
-                <p><? echo $post["content"] ?></p>
-                <small class="blockquote-footer"><? echo $post["created_at"] ?></small>
+            <p><? echo $post["content"] ?></p>
+            <small class="blockquote-footer"><? echo $post["created_at"] ?></small>
         </div>
     </div>
 
